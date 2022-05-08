289d6921dc6566465140e90b52be55be087c5f02
열일 <isawthat77@naver.com>
2022-05-09 02:32:30 +0900

    20220509

---
+++ search-programming-word/App.js
@@ -0,0 +1,16 @@
+import SearchInput from "./SearchInput.js";
+
+export default function App(target){
+    this.state={
+        fetchedLanguages : [],
+        selectedLanuages : []
+    }
+    
+    this.setState = (newxState) =>{
+
+    }
+
+    const searchInput = new SearchInput(target, '')
+    
+}
+
